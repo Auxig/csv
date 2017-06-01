d24523094d01482e92cc940479bcd72cbb820c63
Auxi <auxi@localhost.localdomain>
2017-06-01 15:25:15 +0200

    cambios

--- src/test/java/SuperCSV.java
+++ src/test/java/SuperCSV.java
@@ -19,7 +19,8 @@ public class SuperCSV {
 
             // Simulación de lo que responde plataforma frente a la petición de search
             // De la clase AbstractHTTPMethod, método readResponse recuperamos el csv con el inputStream
-            FileInputStream in = new FileInputStream("/home/auxi/Documentos/usuarios.csv");
+//            FileInputStream in = new FileInputStream("/home/auxi/Documentos/usuarios.csv");
+            FileInputStream in = new FileInputStream("/home/auxi/Documentos/develop/csv/src/main/resources/usuarios.csv");
             InputStreamReader input = new InputStreamReader(in);
 
             ICsvMapReader mapReaderRespuesta = new CsvMapReader(input, (new CsvPreference.Builder('"', 59, "\r\n")).build());
@@ -32,13 +33,16 @@ public class SuperCSV {
             List<Map<String, String>> listmapRespuesta = new LinkedList<Map<String, String>>();
 
             Map<String, String> customerMapRespuesta;
+            System.out.println("*****************************************************************************************");
+            System.out.println("USUARIOS DE LA RESPUESTA A LA PETICIÓN");
             while( (customerMapRespuesta = mapReaderRespuesta.read(header)) != null ) {
-//                System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReaderRespuesta.getLineNumber(),
-//                        mapReaderRespuesta.getRowNumber(), customerMapRespuesta));
+                System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReaderRespuesta.getLineNumber(),
+                        mapReaderRespuesta.getRowNumber(), customerMapRespuesta));
                 listmapRespuesta.add(customerMapRespuesta);
             }
+            System.out.println("*****************************************************************************************");
 
-            System.out.println("Lista de respuesta: " +listmapRespuesta.toString());
+//            System.out.println("Lista de respuesta: " +listmapRespuesta.toString());
 
 //            System.out.println("*****************************************************************************************");
 //            System.out.println("USUARIOS DE LA RESPUESTA A LA PETICIÓN");
@@ -56,7 +60,7 @@ public class SuperCSV {
             // Simulación de lo esperado en el escenario para comprobar que está contenido en la respuesta anterior
 
 
-            ICsvMapReader mapReaderEsperado = new CsvMapReader(new FileReader("/home/auxi/Documentos/usuarios_import.csv"), (new CsvPreference.Builder('"', 59, "\r\n")).build());
+            ICsvMapReader mapReaderEsperado = new CsvMapReader(new FileReader("/home/auxi/Documentos/develop/csv/src/main/resources/usuarios_import.csv"), (new CsvPreference.Builder('"', 59, "\r\n")).build());
 //            ICsvMapReader mapReaderEsperado = new CsvMapReader(new FileReader("/home/auxi/Documentos/usuarios_import.csv"), CsvPreference.STANDARD_PREFERENCE);
 
             final String[] headerEsperada = mapReaderEsperado.getHeader(true);
@@ -64,18 +68,16 @@ public class SuperCSV {
             List<Map<String, String>> listmapEsperada = new LinkedList<Map<String, String>>();
 
             Map<String, String> customerMapEsperado;
+            System.out.println("*****************************************************************************************");
+            System.out.println("USUARIOS QUE ESPERAMOS");
             while( (customerMapEsperado = mapReaderEsperado.read(headerEsperada)) != null ) {
-//                System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReaderEsperado.getLineNumber(),
-//                        mapReaderEsperado.getRowNumber(), customerMapEsperado));
+                System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReaderEsperado.getLineNumber(),
+                        mapReaderEsperado.getRowNumber(), customerMapEsperado));
                 listmapEsperada.add(customerMapEsperado);
 
-//                if (listmapRespuesta.contains(customerMapEsperado)) {
-//                    System.out.println("El usuario esperado " + customerMapEsperado.get("Nombres") + " SI está contenido en la respuesta");
-//                } else {
-//                    System.out.println("El usuario esperado " + customerMapEsperado.get("Nombres") + " NO está contenido en la respuesta");
-//                }
             }
-            System.out.println("Lista esperada: " +listmapEsperada.toString());
+            System.out.println("*****************************************************************************************");
+//            System.out.println("Lista esperada: " +listmapEsperada.toString());
 
             if (listmapEsperada.size() > listmapRespuesta.size()) {
                 System.out.println("ERROR .- Hay más usuarios esperados que recibidos");
